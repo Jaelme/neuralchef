383646f158938228e1482fc626d1b80c173665b2
Jakob Elmecker <jakob14.e@gmail.com>
2025-05-05 09:18:46 +0200

    Initial commit

---
+++ src/main/java/com/devportfolio/neuralchef/NeuralchefApplication.java
@@ -0,0 +1,13 @@
+package com.devportfolio.neuralchef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralchefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuralchefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devportfolio/neuralchef/NeuralchefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devportfolio.neuralchef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuralchefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
